0f9eeac10d0a4666c631fb93eae8cef9901ecf1e
gerardooss <marcelinus.gerardo@ui.ac.id>
2025-03-25 20:21:28 +0700

    Initial commit

---
+++ GreenDirectory/GreenDirectory/GreenDirectoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenDirectoryApp.swift
+//  GreenDirectory
+//
+//  Created by Marcelinus Gerardo on 25/03/25.
+//
+
+import SwiftUI
+
+@main
+struct GreenDirectoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
