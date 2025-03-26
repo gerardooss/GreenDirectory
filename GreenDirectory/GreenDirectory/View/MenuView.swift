a01c557bb8efb9aea6c63365f17a66c5bd9c5627
Georgius Kenny Gunawan <georgiuskenny@10-60-54-69.0200.bsd.id.iosda.org>
2025-03-26 09:53:58 +0700

    add: menu page view

---
+++ GreenDirectory/GreenDirectory/View/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  GreenDirectory
+//
+//  Created by Georgius Kenny Gunawan on 26/03/25.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
