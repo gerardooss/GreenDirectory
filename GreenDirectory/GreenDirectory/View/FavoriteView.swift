48447fd669959db6cd410929657787cc51677698
Geokenny23 <kennygun2308@gmail.com>
2025-04-08 07:45:44 +0700

    rapihin code

--- GreenDirectory/GreenDirectory/View/FavoriteView.swift
+++ GreenDirectory/GreenDirectory/View/FavoriteView.swift
@@ -4,7 +4,8 @@ import _SwiftData_SwiftUI
 struct FavoriteView: View {
     @State var searchText:String = ""
     @Query(filter: #Predicate<Menu> { $0.isFavorite == true }) private var favoriteMenus: [Menu]
-
+    
+ 
     var groupedFavorites: [Tenant: [Menu]] {
         Dictionary(grouping: favoriteMenus) { $0.tenant! }
     }
@@ -21,7 +22,7 @@ struct FavoriteView: View {
         NavigationStack {
             ScrollView{
                 ForEach(filteredFavorites.keys.sorted(by: { $0.name < $1.name }), id: \.id) { tenant in
-                    if let menus = filteredFavorites[tenant] {
+                    if let menus = filteredFavorites[tenant]{
                         VStack(alignment: .leading) {
                             Text(tenant.name)
                                 .font(.headline)
